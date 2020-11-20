2a8a131c25d4f41ba875197595c18736e4dae6a6
ms2geki <mano-syou@popular-soft.com>
2020-11-20 21:27:33 +0900

    更新

---
+++ angular_form/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { NameEditorComponent } from "./name-editor/name-editor.component";
+import { ProfileEditorComponent } from "./profile-editor/profile-editor.component";
+
+@NgModule({
+  declarations: [AppComponent, NameEditorComponent, ProfileEditorComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    // other imports ...
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
